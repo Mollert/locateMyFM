ddc2bec0d5c0ee850ceae912fe8236b60f85f93e
Mollert <blueskies@carolina.rr.com>
2019-08-04 20:29:27 -0400

    increased images on screen

--- javascriptIndex.js
+++ javascriptIndex.js
@@ -1,6 +1,6 @@
 
-let imgIds = ["fmA", "fmB", "fmC", "fmD", "fmE", "fmF", "fmG", "fmH", "fmI", "fmJ"];
-let imgSrc = ["./image/markets/fmA.png", "./image/markets/fmB.png", "./image/markets/fmC.png", "./image/markets/fmD.png", "./image/markets/fmE.png", "./image/markets/fmF.png", "./image/markets/fmG.png", "./image/markets/fmH.png", "./image/markets/fmI.png", "./image/markets/fmJ.png"];
+let imgIds = ["fmA", "fmB", "fmC", "fmD", "fmE", "fmF", "fmG", "fmH", "fmI", "fmJ", "fmK", "fmL", "fmM", "fmN", "fmO", "fmP"];
+let imgSrc = ["./image/markets/fmA.png", "./image/markets/fmB.png", "./image/markets/fmC.png", "./image/markets/fmD.png", "./image/markets/fmE.png", "./image/markets/fmF.png", "./image/markets/fmG.png", "./image/markets/fmH.png", "./image/markets/fmI.png", "./image/markets/fmJ.png", "./image/markets/fmK.png", "./image/markets/fmL.png", "./image/markets/fmM.png", "./image/markets/fmN.png", "./image/markets/fmO.png", "./image/markets/fmP.png"];
 
 const randomizeSolutions = (defaultArray) => {		
 	let randomizedArray = [];
@@ -18,20 +18,22 @@ const randomizeSolutions = (defaultArray) => {
 
 const reorderImg = () => {
 	let visibleWidth = document.body.clientWidth - 17;
-	imgIds = randomizeSolutions(imgIds);	
+	imgIds = randomizeSolutions(imgIds);
 	let inRow = 2;
 	if (visibleWidth <= 800) {
 		inRow = 2;
 	} else if (visibleWidth > 800 && visibleWidth <= 1200) {
 		inRow = 3;
 		let remove1 = imgIds.pop();
-		document.getElementById(remove1).style.display = "none";	
-	} else {
-		inRow = 4;
-		let remove1 = imgIds.pop();
 		document.getElementById(remove1).style.display = "none";
 		let remove2 = imgIds.pop();
 		document.getElementById(remove2).style.display = "none";
+		let remove3 = imgIds.pop();
+		document.getElementById(remove3).style.display = "none";
+		let remove4 = imgIds.pop();
+		document.getElementById(remove4).style.display = "none";	
+	} else {
+		inRow = 4;	
 	}
 	let imgW = (visibleWidth / inRow).toFixed(2);
 	let imgH = (imgW * 2 / 3).toFixed(2);
@@ -46,14 +48,14 @@ const reorderImg = () => {
 	let tabletH = (allImgH - (visibleWidth * .2)).toFixed(2);
 	document.getElementById("tablet").style.height = (tabletH + "px");
 }
-
+/*
 let overall = window.innerWidth;
 console.log(overall);
 let subtract = document.body.clientWidth;
 console.log(subtract);
 let ans = (overall - subtract);
 console.log(ans);
-
+*/
 
 reorderImg();
 /*
