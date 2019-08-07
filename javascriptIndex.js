128ce160217e8be10823f15724e07563374ff284
Mollert <blueskies@carolina.rr.com>
2019-08-06 20:31:47 -0400

    Developing responsive front page

--- javascriptIndex.js
+++ javascriptIndex.js
@@ -1,23 +1,23 @@
 
 let imgIds = ["fmA", "fmB", "fmC", "fmD", "fmE", "fmF", "fmG", "fmH", "fmI", "fmJ", "fmK", "fmL", "fmM", "fmN", "fmO", "fmP"];
-let imgSrc = ["./image/markets/fmA.png", "./image/markets/fmB.png", "./image/markets/fmC.png", "./image/markets/fmD.png", "./image/markets/fmE.png", "./image/markets/fmF.png", "./image/markets/fmG.png", "./image/markets/fmH.png", "./image/markets/fmI.png", "./image/markets/fmJ.png", "./image/markets/fmK.png", "./image/markets/fmL.png", "./image/markets/fmM.png", "./image/markets/fmN.png", "./image/markets/fmO.png", "./image/markets/fmP.png"];
+let imgSrc = ["./image/markets/fmA.png", "./image/markets/fmB.png", "./image/markets/fmC.png", "./image/markets/fmD.png", "./image/markets/fmE.png", "./image/markets/fmF.png", "./image/markets/fmG.png", "./image/markets/fmH.png", "./image/markets/fmI.png", "./image/markets/fmJ.png", "./image/markets/fmK.png", "./image/markets/fmL.png", "./image/markets/fmM.png", "./image/markets/fmN.png", "./image/markets/fmO.png", "./image/markets/fmP.png", "./image/markets/fmQ.png", "./image/markets/fmR.png", "./image/markets/fmS.png", "./image/markets/fmT.png", "./image/markets/fmU.png", "./image/markets/fmV.png", "./image/markets/fmW.png", "./image/markets/fmX.png", "./image/markets/fmY.png", "./image/markets/fmZ.png"];
 
-const randomizeSolutions = (defaultArray) => {		
-	let randomizedArray = [];
-	let originalLength = defaultArray.length;
+const randomImgSelection = (arrayLength) => {		
+	let randomArray = [];
 
-	for ( let i = 1 ; i < originalLength ; i++ ) {
-		let subtracter = Math.floor((Math.random() * defaultArray.length));
-		randomizedArray.push(defaultArray[subtracter]);
-		defaultArray.splice(subtracter, 1);
+	for ( let i = 0 ; i < arrayLength ; i++ ) {
+		let subtracter = Math.floor((Math.random() * 26));
+		randomArray.push(imgSrc[subtracter]);
+		imgSrc.splice(subtracter, 1);
 	}
-	randomizedArray.push(defaultArray[0]);
 
-	return randomizedArray;
+	return randomArray;
 };
 
 const reorderImg = () => {
-	let visibleWidth = document.body.clientWidth - 17;
+//	let visibleWidth = window.innerWidth;
+	let visibleWidth = document.body.clientWidth;
+//	let visibleWidth = document.body.clientWidth - 17;
 	imgIds = randomizeSolutions(imgIds);
 	let inRow = 2;
 	if (visibleWidth <= 800) {
@@ -57,7 +57,7 @@ let ans = (overall - subtract);
 console.log(ans);
 */
 
-reorderImg();
+//reorderImg();
 /*
 window.onresize = () => {
 	setTimeout(() => {
@@ -65,3 +65,70 @@ window.onresize = () => {
 	}, 500);
 };
 */
+//console.log(window.innerWidth);
+console.log(document.body.clientWidth);
+
+//let tabWidth = ((window.innerWidth) * 0.7).toFixed(2);
+let displayWidth = document.body.clientWidth;
+let tabWidth = ((displayWidth + 17.0) * 0.7).toFixed(2);
+let lookWidth = tabWidth - 96.0;
+//Height of "looking" text
+let lookNumChar = (document.getElementById("looking").textContent).length;
+console.log(tabWidth + " " + lookWidth + " " + lookNumChar);
+let lookLineLength = (lookNumChar * 16.2).toFixed(2);
+
+let numLookLines = Math.ceil(lookLineLength / lookWidth);
+
+//console.log(lookLineLength + ' = lenght of "Look Line"');
+//console.log(numLookLines + " = number of lines for this page width");
+
+let explainWidth = tabWidth - 64.0;
+let explainNumChar = (document.getElementById("explain").textContent).length;
+let explainLineLength = (explainNumChar * 10.8).toFixed(2);
+
+let numExplainLines = Math.ceil(explainLineLength / explainWidth);
+console.log(numExplainLines);
+
+
+let quoteWidth = tabWidth - 32.0;
+let quoteNumChar = (document.getElementById("quote").textContent).length;
+let quoteLineLength = (quoteNumChar * 11.2).toFixed(2);
+
+let numQuoteLines = Math.ceil(quoteLineLength / quoteWidth);
+console.log(numQuoteLines);
+
+
+let lookHeight = (numLookLines * 43) + 21.4 + 21.4 + 16 + 16;
+console.log(lookHeight);
+
+let explainHeight = numExplainLines * 26;
+console.log(explainHeight);
+
+let quoteHeight = (numQuoteLines * 30) + 32 + 32;
+console.log(quoteHeight);
+
+let tabletHeight = lookHeight + explainHeight + quoteHeight;
+document.getElementById("tablet").style.height = tabletHeight;
+console.log(tabletHeight);
+
+let inRow = 2;
+if (displayWidth <= 700) {
+	inRow = 2;
+} else if (displayWidth > 700 && displayWidth <= 1000) {
+	inRow = 3;
+} else if (displayWidth > 1000 && displayWidth <= 1400) {
+	inRow = 4;
+} else {
+	inRow = 5;
+}
+
+let imgW = (displayWidth / inRow).toFixed(2);
+let imgH = (imgW * 2 / 3).toFixed(2);
+
+let numRows = Math.ceil((tabletHeight + (displayWidth * .10)) / imgH);
+
+console.log(numRows);
+
+let numImgs = inRow * numRows;
+let theseImgs = [];
+theseImgs = randomImgSelection(numImgs);
